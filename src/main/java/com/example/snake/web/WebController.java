ad3dfb44e51bef74759c3eab8b05adc826e6131d
Adilet <usenkanov.adilet.kaantbekovich@gmail.com>
2024-05-08 16:33:48 +0600

    First commit

---
+++ src/main/java/com/example/snake/web/WebController.java
@@ -0,0 +1,16 @@
+package com.example.snake.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Обработчик web-запросов
+ */
+@RestController
+public class WebController {
+    @GetMapping("/hello")
+    public String User(@RequestParam (value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
